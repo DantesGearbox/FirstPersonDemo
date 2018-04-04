f5ec67df2ff408d0f42126adb6de6974c0dea448
DantesGearbox <mads.engberg@hotmail.com>
2018-04-04 18:20:28 +0200

    Working movement and events

---
+++ FirstPersonController/Assets/Scripts/ActivateByEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateByEvent : MonoBehaviour {
+
+    private void OnEnable() {
+        EventManager.OnEnter += DoSomething;
+    }
+
+    private void OnDisable() {
+        EventManager.OnEnter -= DoSomething;
+    }
+
+    private void DoSomething() {
+        Debug.Log("Event fired and seen");
+    }
+}
